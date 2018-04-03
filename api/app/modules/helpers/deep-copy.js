6bea94f8139bc1ed19b7d12143c056a432d51df4
James <jknight@lunenfeld.ca>
2018-04-03 10:09:04 -0400

    Remove api
    - move api to separate repo

--- api/app/modules/helpers/deep-copy.js
+++
@@ -1,8 +0,0 @@
-// deep copy a variable
-const DeepCopy = (arr) => {
-  if (!arr) {
-    return null;
-  }
-  return JSON.parse(JSON.stringify(arr));
-};
-module.exports = DeepCopy;
--- api/config.test.js
+++
@@ -1,8 +0,0 @@
-const Config = require('./config');
-
-describe('dev config', () => {
-  test('load config based on environment', () => {
-    // ensure settings object is correct
-    expect(Config.env).toBe('test');
-  });
-});
