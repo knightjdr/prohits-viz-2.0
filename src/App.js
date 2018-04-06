b3e249ea71ae8d3f47f5bc356f0d555b4e1d998d
James <jknight@lunenfeld.ca>
2018-04-06 16:24:07 -0400

    Add getting started arrow
    - add prompt on home page to let users know to scroll

--- src/App.js
+++ src/App.js
@@ -1,19 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import About from './about/about';
+import ArrowPrompt from './home/arrow-prompt/arrow-prompt';
 import Home from './home/home-container';
 import Spotlight from './spotlight/spotlight';
 import Tools from './tools/tools';
 
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <Home />
-    <Tools />
-    <Spotlight />
-    <About />
-  </div>
-);
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listenerAdded: false,
+      hidePrompt: true,
+    };
+  }
+  componentDidMount = () => {
+    const { scrollTop } = this.elem;
+    if (scrollTop === 0) {
+      this.elem.addEventListener('scroll', this.handleScroll);
+      this.setState({
+        hidePrompt: false,
+        listenerAdded: true,
+      });
+    }
+  }
+  componentWillUnmount = () => {
+    if (this.state.listenerAdded) {
+      this.elem.removeEventListener('scroll', this.handleScroll);
+    }
+  }
+  checkTop = (scrollTop) => {
+    if (
+      !this.state.hidePrompt &&
+      scrollTop > 0
+    ) {
+      this.elem.removeEventListener('scroll', this.handleScroll);
+      this.setState({
+        hidePrompt: true,
+        listenerAdded: false,
+      });
+    }
+  }
+  handleScroll = () => {
+    const { scrollTop } = this.elem;
+    this.checkTop(scrollTop);
+  }
+  render() {
+    return (
+      <div
+        className="App"
+        ref={(elem) => { this.elem = elem; }}
+      >
+        <Home />
+        <Tools />
+        <Spotlight />
+        <About />
+        <ArrowPrompt hide={this.state.hidePrompt} />
+      </div>
+    );
+  }
+}
 
 export default App;
